bd9ba1a5a3c1239d243bc940fa354f08540b476a
Dennis de Jesus <ddejesus@outlook.com>
2022-02-18 22:16:00 +0000

    Configure HMR on Gitpod & localhost.

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,18 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		vite: {
+			server: {
+				hmr: {
+					clientPort: process.env.HMR_HOST ? 443: 3000,
+					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+				}
+			}
+		}
 	}
 };
 
