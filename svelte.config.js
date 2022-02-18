d0768bf2ea83f3118476971383b4b3fb2219910f
Dennis de Jesus <ddejesus@outlook.com>
2022-02-18 22:29:13 +0000

    Removed target from svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,13 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+
+		vite: {
+			server: {
+				hmr: {
+					clientPort: process.env.HMR_HOST ? 443: 3000,
+					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
 	}
 };
 
