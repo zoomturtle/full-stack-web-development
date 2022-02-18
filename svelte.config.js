acc82327a6b04a03b0901d8a6ac16a52006998d2
Dennis de Jesus <ddejesus@outlook.com>
2022-02-18 22:33:31 +0000

    Changed svelte.config.js again

--- svelte.config.js
+++ svelte.config.js
@@ -15,6 +15,9 @@ const config = {
 				hmr: {
 					clientPort: process.env.HMR_HOST ? 443: 3000,
 					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+				}
+			}
+		}
 	}
 };
 
